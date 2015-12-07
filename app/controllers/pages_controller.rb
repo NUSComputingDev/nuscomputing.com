dd37cd1f60103e01936b2f55b458809f73352fb1
Nicholas Lum <nicholaslum444@hotmail.com>
2015-12-08 03:49:26 +0800

    Added Stuff for easy setup
    Pages controller with relevant actions
    Routing
    Gem for mysql

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def home
+  end
+
+  def about
+  end
+
+  def events
+  end
+
+  def connect
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,21 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+
+  get 'pages/home'
+
+  get 'pages/about'
+
+  get 'pages/events'
+
+  get 'pages/connect'
+
+  # Rails will auto put new routes above this
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'pages#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
