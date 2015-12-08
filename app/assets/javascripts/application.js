5989969b1d5c8ed8037df34744a2e1eb981663dd
Nicholas Lum <nicholaslum444@hotmail.com>
2015-12-08 21:25:19 +0800

    Removed jquery ui
    not needed now

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery-ui 
 //= require jquery
 //= require jquery_ujs
 //= require foundation
