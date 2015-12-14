09a7d05a2e9372eed0df8f0254e779f802255c2a
landuomu <lhchchc@gmail.com>
2015-12-14 19:35:17 +0800

    Change datatype of contact in enquiries from integer to string

--- app/models/enquiry.rb
+++ app/models/enquiry.rb
@@ -1,6 +1,5 @@
 class Enquiry < ActiveRecord::Base
 	validates :name, presence: true
 	validates :email, presence: true, format: /\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}\b/
-	validates :contact, length: 5..8
 	validates :message, presence: true
 end
