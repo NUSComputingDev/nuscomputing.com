c2257a7fe319d46db4d43102f6417e921a17902b
landuomu <lhchchc@gmail.com>
2016-01-05 22:41:48 +0800

    add description column to sponsors db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151223070523) do
+ActiveRecord::Schema.define(version: 20160105104020) do
 
   create_table "enquiries", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -55,11 +55,12 @@ ActiveRecord::Schema.define(version: 20151223070523) do
   end
 
   create_table "sponsors", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "link",       limit: 255
-    t.string   "img",        limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "name",        limit: 255
+    t.string   "link",        limit: 255
+    t.string   "img",         limit: 255
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.text     "description", limit: 65535
   end
 
 end
