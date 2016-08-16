5cd1655ff40203a97adbdf6d45a5feafd5ba854a
Nicholas Lum <nicholaslum444@hotmail.com>
2016-08-17 04:04:05 +0800

    Add recaptcha support for enquiry form

--- app/assets/javascripts/connect.js
+++ app/assets/javascripts/connect.js
@@ -1,36 +1,44 @@
 $(document).ready(function() {
-	// add event listeners to display user feedback
-	var intervalDots, dotCount = 0;
-  $(".enquiry-form").on("ajax:send", function (e, xhr) {
-  	$("#loadingModal").foundation("reveal", "open");
-  	intervalDots = setInterval(function () {
-  		// add dot to message every second (up to 3 dots)
-  		if (dotCount == 3) {
-  			$(".loading-message").text("Submitting enquiry, please wait");
-  			dotCount = 0;
-  		} else {
-  			$(".loading-message").append(".");
-  			dotCount++;
-  		}
-  	}, 1000);
-  })
-  .on("ajax:success", function (e, data, status, xhr) {
-  	$("#loadingModal").foundation("reveal", "close");
-  	clearInterval(intervalDots);
-  	$(".complete-message").text("Enquiry submitted!");
-  	$("#completeModal").foundation("reveal", "open");
-  	// reset form
-  	$(".enquiry-form").trigger("reset");
-  })
-  .on("ajax:error", function (e, xhr, status, error) {
-  	$("#loadingModal").foundation("reveal", "close");
-  	clearInterval(intervalDots);
-  	$(".complete-message").text("Enquiry failed! Please try again...");
-  	$("#completeModal").foundation("reveal", "open");
-  	// keep form data in UI intact
-  });
-  
-  $('a.close').on('click', function() {
-		$('#completeModal').foundation('reveal', 'close');
-	});
+    // add event listeners to display user feedback
+    var intervalDots, dotCount = 0;
+    $(".enquiry-form").on("ajax:send", function (e, xhr) {
+        $("#loadingModal").foundation("reveal", "open");
+        intervalDots = setInterval(function () {
+            // add dot to message every second (up to 3 dots)
+            if (dotCount == 3) {
+                $(".loading-message").text("Submitting enquiry, please wait");
+                dotCount = 0;
+            } else {
+                $(".loading-message").append(".");
+                dotCount++;
+            }
+        }, 1000);
+    })
+        .on("ajax:success", function (e, data, status, xhr) {
+            $("#loadingModal").foundation("reveal", "close");
+            clearInterval(intervalDots);
+            $(".complete-message").html("<h3>Enquiry submitted!</h3><p>We will get back to you in a few days time.</p>");
+            $("#completeModal").foundation("reveal", "open");
+            // reset form
+            $(".enquiry-form").trigger("reset");
+        })
+        .on("ajax:error", function (e, xhr, status, error) {
+            $("#loadingModal").foundation("reveal", "close");
+            clearInterval(intervalDots);
+            $(".failure-message").html("<h3>Enquiry submission failed!</h3><p>Did you complete the captcha? Please check the enquiry form again.</p>");
+            $("#failureModal").foundation("reveal", "open");
+            // keep form data in UI intact
+        });
+
+    $('a.complete-close').on('click', function() {
+        $('#completeModal').foundation('reveal', 'close');
+    });
+
+    $('a.failure-close').on('click', function() {
+        $('#failureModal').foundation('reveal', 'close');
+    });
+
+    $(document).on('closed.fndtn.reveal', '#completeModal[data-reveal]', function() {
+        location.reload();
+    });
 });
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -37,13 +37,22 @@ class PagesController < ApplicationController
 
 	def enquiry
 		@enquiry = Enquiry.new(enquiry_params)
-    # send mail before saving to database.
-    # because I think it's more likely that the mailing fails than it can't save to db
-    # also to prevent multiple entries in db.
-		if EnquiryMailer.notify(@enquiry).deliver_now
-      @enquiry.save
+
+		respond_to do |format|
+			if verify_recaptcha(model: @enquiry)
+        logger.fatal('capctha pass')
+				# send mail before saving to database.
+				# because I think it's more likely that the mailing fails than it can't save to db
+				# also to prevent multiple entries in db.
+				if EnquiryMailer.notify(@enquiry).deliver_now
+					@enquiry.save
+				end
+        format.html { redirect_to connect_path, notice: 'Enquiry Submitted!' }
+
+			else
+				logger.fatal('capctha fail')
+			end
 		end
-		render 'connect'
 	end
 
 	private
