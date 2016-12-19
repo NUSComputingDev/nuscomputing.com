c842241b7dc01760462a9958723811bee5bd5c8f
Jia Yee <jiayeerawr@gmail.com>
2016-12-19 17:53:11 +0800

    Add data validation for products

--- db/migrate/20161216064230_create_products.rb
+++ db/migrate/20161216064230_create_products.rb
@@ -1,13 +1,15 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-      t.string :name
+      t.string :name, null: false, presence: true
       t.text :images
       t.text :description
       t.text :options
-      t.string :price
-      t.string :stock
-      t.string :orders
+      t.string :price, null: false, presence: true
+      t.string :stock, null: false, presence: true,
+        numericality: { only_integer: true }
+      t.string :orders, null: false, presence: true,
+        numericality: { only_integer: true }
 
       t.timestamps null: false
     end
